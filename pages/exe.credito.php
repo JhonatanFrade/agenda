1240bd65ad2de2b8ae348b335b9fb8c7cf236f04
LeonardoMeirelles <44680355+LeonardoMeirelles@users.noreply.github.com>
2018-11-16 20:44:27 -0200

    Update exe.credito.php

--- pages/exe.credito.php
+++ pages/exe.credito.php
@@ -77,72 +77,114 @@
   <input type="hidden" name="tipo_mov" value="1" />
 </form>
 
-<br><br><br><br>
+<br><br><br>
 
-<div class="dropdown col-md-4">
-  <label>Carteiras</label>
-  <select name="id_conta" class="form-control selectContaListagem">
-    <?php 
-        $carteiras = $CarteirasDAO->listar();
-      foreach ($carteiras as $key => $obj) {
-        $id = $obj->getId();
-        $name = $obj->getNome();
-      ?>
-    <option value="<?php echo $id ?>"><?php echo $name ?></option>
-    <?php } ?>
-  </select>
-</div>
-<br>
-<div class="form-group col-md-10">
-  <label>Listagem de créditos</label>
-  <table class="table table-striped">
-    <thead>
-      <tr>
-        <th scope="col">Data</th>
-        <th scope="col">Centro de Custo</th>
-        <th scope="col">Valor</th>
-        <th scope="col">Ações</th>
-      </tr>
-    </thead>
-    <tbody>
+<!--  Formulário número 2-->
+<form name = "form2" action="php/acao.credito.php?action=select" method="POST">
+  <div class="dropdown col-md-4">
+    <label>Carteiras</label>
+    <select name="id_conta2" class="form-control"> 
+    <!-- <select name="id_conta" class="form-control selectContaListagem"> -->
+      <!--Quando selecionado a opção a id da conta é mandada para o js, para o evento change
+      que por sua vez manda a id_conta e o tipo de movimentação para o arquivo acao.credito.php  -->
       <?php 
-
-        $tipoDeListagem = 1;
-
-        $creditos = $MovimentacaoDAO->listar($tipoDeListagem);
-        if(!empty($creditos)){
-          foreach ($creditos as $key => $obj) {
-          $data = $obj->getData();
-
-          $id_centro_custos = $obj->getId_centro_custos();
-          $centro_custos = new CentroDeCustos();
-          $centro_custos->setId($id_centro_custos);
-          $centro_de_custos = $CentroDeCustosDAO->listarUmCentroDeCusto($centro_custos);
-          foreach ($centro_de_custos as $k => $objeto) {
-            $descricao_centro_custos = $objeto->getNome();
-          }
-
-          $valor = $obj->getValor();
-          $id_credito = $obj->getId();
-      ?>
-      <tr>
-        <td><?php echo $data;?></td>
-        <td><?php echo $descricao_centro_custos;?></td>
-        <td><?php echo 'R$' . number_format($valor, 2, ',', '.');?></td>
-        <td>
-          <a href="index.php?pag=credito.edit&id=<?php echo $id_credito; ?>">
-            <i class="fa fa-pencil-alt"></i>
-          </a>
-          <a href="php/acao.credito.php?action=delete&id=<?php echo $id_credito; ?>">
-            <i class="fa fa-trash"></i>
-          </a>
-        </td>
-      </tr>
+          $carteiras = $CarteirasDAO->listar();
+        foreach ($carteiras as $key => $obj) {
+          $id = $obj->getId();
+          $name = $obj->getNome();
+        ?>
+      <option value="<?php echo $id ?>"><?php echo $name ?></option>
       <?php } ?>
-      <?php }else{ ?>
-         <td><?php echo 'Sem registro!';?></td>
-        <?php } ?>
-    </tbody>
-  </table>
-</div>
+    </select>
+    <small class="form-text text-muted">Selecione a carteira dejeda e clique em filtrar
+    para que seja realizada a filtragem na tabela</small>
+  </div>
+  <br>
+  <div class="form-group col-md-6">
+    <button type="submit" class="btn btn-primary">Filtrar</button>
+  </div>
+</form>
 
+  <br>
+
+  <div class="form-group col-md-10 ">
+    <label>Listagem de créditos</label>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Data</th>
+          <th scope="col">Centro de Custo</th>
+          <th scope="col">Valor</th>
+          <th scope="col">Ações</th>
+        </tr>
+      </thead>
+      <!-- <tbody id="listaConta"> -->
+      <tbody>
+        <?php 
+
+          
+
+          //$creditos = $MovimentacaoDAO->listar($tipoDeListagem);
+          
+
+          if (isset($_GET['conta']) and !empty($_GET['conta'])) 
+          {
+
+            $credito_selec = $_GET['conta'];
+
+            $tipoDeListagem = 1;
+
+            $tipoList = new Movimentacao();
+
+            $tipoList ->setTipo_mov($tipoDeListagem);
+
+            $creditos = $MovimentacaoDAO->listar($tipoList);
+
+            //echo($creditos);
+            //exit;
+
+            /*
+            print_r($creditos);
+            
+            */
+
+            foreach ($creditos as $key => $obj) {
+
+            $id = $obj->getId_conta();
+            $tipo = $obj->getTipo_mov();
+
+            if (($id == $credito_selec) && ($tipo == $tipoDeListagem)) 
+            {
+              $data = $obj->getData();
+              $id_centro_custos = $obj->getId_centro_custos();
+              $centro_custos = new CentroDeCustos();
+              $centro_custos->setId($id_centro_custos);
+              $centro_de_custos = $CentroDeCustosDAO->listarUmCentroDeCusto($centro_custos);
+              foreach ($centro_de_custos as $k => $objeto) {
+                $descricao_centro_custos = $objeto->getNome();
+              }
+
+              $valor = $obj->getValor();
+              $id_credito = $obj->getId();
+        ?>
+        <tr>
+          <td><?php echo $data;?></td>
+          <td><?php echo $descricao_centro_custos;?></td>
+          <td><?php echo 'R$' . number_format($valor, 2, ',', '.');?></td>
+          <td>
+            <a href="index.php?pag=credito.edit&id=<?php echo $id_credito; ?>">
+              <i class="fa fa-pencil-alt"></i>
+            </a>
+            <a href="php/acao.credito.php?action=delete&id=<?php echo $id_credito; ?>">
+              <i class="fa fa-trash"></i>
+            </a>
+          </td>
+        </tr>
+        <?php } ?>
+        <?php }
+        }else{ ?>
+           <td><?php echo 'Sem registro!';?></td>
+          <?php } ?>
+      </tbody>
+    </table>
+  </div>
