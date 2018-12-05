bff1f6aa84d18d0c1fca7db322a96efcf195c566
LeonardoMeirelles <44680355+LeonardoMeirelles@users.noreply.github.com>
2018-12-05 13:21:49 -0200

    Update class.RecorrentesDAO.php

--- Recorrentes/class.RecorrentesDAO.php
+++ Recorrentes/class.RecorrentesDAO.php
@@ -1,6 +1,23 @@
 <?php
-// Jonhatan desenvolveu está classe.
-require_once('class.DbAdmin.php');
+
+/*
+ - Jonhatan Desenvolveu está classe
+
+ - Dia 30/11/2018 Leonardo modificou a classe
+     Métodos Alterados:
+       - cadastrar
+       - listar
+       
+ - Dia 04/12/2018 Leonardo modificou a classe
+     Métodos Alterados:
+      - excluir
+      - atualiza
+     Método Adicionado:
+      - listarUmaRecorrente
+*/
+
+require_once('C:/xampp/htdocs/ULBRA/web2/agenda/DbAdmin/class.DbAdmin.php');
+
 
 class RecorrentesDAO{
 
@@ -10,13 +27,13 @@ class RecorrentesDAO{
 
 		$dba = new DbAdmin('mysql');
 
-		$dba->connect('localhost','root','','web2');
+		$dba->connect('localhost','root','','oc_agenda_web_2');
 
 		$this->dba = $dba;
 
 	}
 
-	public function cadastra($objeto){
+	public function cadastrar($objeto){
 
 		$dba = $this->dba;
 
@@ -28,17 +45,18 @@ class RecorrentesDAO{
 		$descricao = $objeto->getDescricao();
 		$valor = $objeto->getValor();
 
+		
 		$query = 'INSERT INTO recorrentes
-					(tipo_mov,dia,descricao,valor)
-				  VALUES ("'.$tipo_mov.'","'.$dia.'","'.$descricao.'","'.$valor.'")';
-				  // A id não será armazenada primária e secundaria não serão armazenadas.
+					(id_centro_custos, id_conta, tipo_mov, dia, descricao, valor)
+				  VALUES ("'.$id_centro_custos.'", "'.$id_conta.'", "'.$tipo_mov.'","'.$dia.'","'.$descricao.'","'.$valor.'")';
 		
 		$dba->query($query);
 		
 	}
 
-
-	public function excluir($objeto){
+	public function excluir($objeto)
+	{
+		
 
 		$dba = $this->dba;
 
@@ -48,8 +66,6 @@ class RecorrentesDAO{
 				  WHERE id = "'.$id_objt.'"';
 		
 		$dba->query($query);
-		
-
 	}
 
 	public function atualiza($objeto){
@@ -64,15 +80,109 @@ class RecorrentesDAO{
 		$descricao = $objeto->getDescricao();
 		$valor = $objeto->getValor();
 
-		$query = 'UPDATE FROM recorrentes  
-				  SET	tipo_mov ="'.$tipo_mov.'",
+		$query = 'UPDATE  recorrentes  
+				  SET id_centro_custos = "'.$id_centro_custos.'",
+				  id_conta = "'.$id_conta.'",
+				  tipo_mov ="'.$tipo_mov.'",
 				  dia ="'.$dia.'",
-				  descricao ="'.$vencimento.'",
+				  descricao ="'.$descricao.'",
 				  valor ="'.$valor.'"
-				  WHERE id  = "'.$id_objt.'"';
+				  WHERE id  = "'.$id.'"';
 		
 		$dba->query($query);
 	}
+
+	/* 
+		Para identificar e listar as tabelas que condizem com a forma de movimentação, 
+		é filtrado apartir do campo tipo_mov:
+		1 - para Crédito
+		2 - para Débito
+	*/
+
+	public function listar($tipoList)
+	{
+		$tipoDeListagem = $tipoList->getTipo_mov();
+		
+		$dba = $this->dba;
+
+		$vet = array();
+
+		$query = 'SELECT id, id_centro_custos, id_conta, tipo_mov, dia, descricao, valor
+				FROM recorrentes
+				WHERE tipo_mov = "'.$tipoDeListagem.'"'; 
+
+		$res = $dba->query($query);
+
+		$num = $dba->rows($res);
+
+		for ($i=0; $i < $num; $i++) 
+		{ 
+			$id = $dba->result($res, $i, 'id');
+			$id_centro_custos = $dba->result($res, $i, 'id_centro_custos');
+			$id_conta = $dba->result($res, $i, 'id_conta');
+			$tipo_mov = $dba->result($res, $i, 'tipo_mov');
+			$dia = $dba->result($res, $i, 'dia');
+			$descricao = $dba->result($res, $i, 'descricao');
+			$valor = $dba->result($res, $i, 'valor');
+
+			$Recorrentes = new Recorrentes();
+
+			$Recorrentes->setId($id);
+			$Recorrentes->setId_centro_custos($id_centro_custos);
+			$Recorrentes->setId_conta($id_conta);
+			$Recorrentes->setTipo_mov($tipo_mov);
+			$Recorrentes->setDia($dia);
+			$Recorrentes->setDescricao($descricao);
+			$Recorrentes->setValor($valor);
+
+			$vet[] = $Recorrentes;
+
+		}
+
+		return $vet;
+	}
+
+	public function listarUmaRecorrente($obj)
+	{
+		$dba = $this->dba;
+
+		$id = $obj->getId();
+		$tipo_mov = $obj->getTipo_mov();
+
+		$query = 'SELECT id, id_centro_custos, id_conta, tipo_mov, dia, descricao, valor
+				  FROM recorrentes
+				  WHERE id = "'.$id.'"
+				  AND tipo_mov = '.$tipo_mov;
+
+		$res = $dba->query($query);
+
+		$num = $dba->rows($res);
+
+		for ($i=0; $i < $num; $i++)
+		 { 
+			$id = $dba->result($res, $i, 'id');
+			$id_centro_custos = $dba->result($res, $i, 'id_centro_custos');
+			$id_conta = $dba->result($res, $i, 'id_conta');
+			$tipo_mov = $dba->result($res, $i, 'tipo_mov');
+			$dia = $dba->result($res, $i, 'dia');
+			$descricao = $dba->result($res, $i, 'descricao');
+			$valor = $dba->result($res, $i, 'valor');
+
+			$Recorrentes = new Recorrentes();
+
+			$Recorrentes->setId($id);
+			$Recorrentes->setId_centro_custos($id_centro_custos);
+			$Recorrentes->setId_conta($id_conta);
+			$Recorrentes->setTipo_mov($tipo_mov);
+			$Recorrentes->setDia($dia);
+			$Recorrentes->setDescricao($descricao);
+			$Recorrentes->setValor($valor);
+
+			$vet[] = $Recorrentes;
+		}
+
+		return $vet;
+    }
 
 }
 
