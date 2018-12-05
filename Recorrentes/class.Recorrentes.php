95a0afc9e489b28f65597b21399b28faf6f45fca
LeonardoMeirelles <44680355+LeonardoMeirelles@users.noreply.github.com>
2018-12-05 13:13:10 -0200

    Update class.Recorrentes.php

--- Recorrentes/class.Recorrentes.php
+++ Recorrentes/class.Recorrentes.php
@@ -7,6 +7,7 @@
         private $id_conta;
         private $tipo_mov;
         private $dia;
+        private $descricao;
         private $valor;
       
 
@@ -25,14 +26,15 @@
             return $this->id;
         }
 
+
     	public function setId_centro_custos($id_centro_custos)
     	{
-    		$this->$id_centro_custos=$$id_centro_custos;
+    		$this->id_centro_custos = $id_centro_custos;
     	}
 
     	public function getId_centro_custos()
     	{
-    		return $this->$id_centro_custos;
+    		return $this->id_centro_custos;
     	}
 
         public function setId_conta($id_conta)
@@ -42,17 +44,17 @@
 
         public function getId_conta()
         {
-            return $this->$id_conta;
+            return $this->id_conta;
         }
 
         public function setTipo_mov($tipo_mov)
         {
-            $this->tipo_mov=$tipo_mov;
+            $this->tipo_mov = $tipo_mov;
         }
 
         public function getTipo_mov()
         {
-            return $this->$tipo_mov;
+            return $this->tipo_mov;
         }
 
         public function setDia($dia)
@@ -62,7 +64,17 @@
 
         public function getDia()
         {
-            return $this->$dia;
+            return $this->dia;
+        }
+
+        public function setDescricao($descricao)
+        {
+            $this->descricao = $descricao;
+        }
+
+        public function getDescricao()
+        {
+            return $this->descricao;
         }
 
         public function setValor($valor)
@@ -72,7 +84,7 @@
 
         public function getValor()
         {
-            return $this->$valor;
+            return $this->valor;
         }
     }
 ?>
