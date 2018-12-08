8640f0b6c500d30099dc2f11d37195317b1d3333
LeonardoMeirelles <44680355+LeonardoMeirelles@users.noreply.github.com>
2018-12-08 15:07:09 -0200

    Update exe.carteira.php

--- pages/exe.carteira.php
+++ pages/exe.carteira.php
@@ -1,7 +1,7 @@
 <!--
      - Arquivo criado pelo Leonardo dia 16/10.
      - Modificado pelo jonhatan dia 23/10/2018 (Adicionado o Modal e a listagem)
-     - Modificado pelo Jonhatam dia 22/11/18 (Desabilitado o botão excluir da listagem)
+     - Modificado pelo Jonhatam dia 22/11/18 (Retirado o botão excluir da listagem)
      - Modificado pelo jonhatan dia 24/11/2018 (Configuração do Relatório)   
 -->
 
